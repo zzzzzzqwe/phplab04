3f787e1b035c67dbf0015a0234636c65ae88a6b6
zzzzzzqwe <dima.gachayev@gmail.com>
2025-03-23 21:04:49 +0200

    minor changes

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <title>Главная</title>
 </head>
 <body>
-    <h1>Рецепт успешно сохранён!</h1>
-    <a href="/recipe/create.php">Добавить ещё</a>
+    <h1>Добро пожаловать. <br>Желаете добавить рецепт?</h1>
+    <a href="/recipe/create.php">Добавить рецепт.</a>
 </body>
 </html>
